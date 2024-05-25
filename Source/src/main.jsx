82c2193d2aed843cfcdab48a773ad0120bf5cd73
Naveen <naveenrajanm9@gmail.com>
2024-05-25 16:41:04 +0530

    metamask

--- Source/src/main.jsx
+++ Source/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { ContractContextProvider } from '../context/ContractContext.jsx'
 import './Global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ContractContextProvider>
+      <App />
+    </ContractContextProvider>
   </React.StrictMode>,
 )
