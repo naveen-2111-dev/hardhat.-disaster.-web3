604b7f672bd5bb57743467c2be35f723b1333ad5
Naveen <naveenrajanm9@gmail.com>
2024-06-05 22:47:37 +0530

    connect button ui update

--- Source/components/connect.jsx
+++ Source/components/connect.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import MetaMask from "../public/image.png"
+import { useContract } from '../context/ContractContext'
 
 const Connect = () => {
+
   return (
     <div className='flex justify-center items-center'>
-      connect
+      <div className='border shadow-2xl rounded-md transform hover:scale-110 transition duration-200 ease-in-out cursor-pointer p-3'>
+        <img src={ MetaMask } width={200} height={100} />
+        <p className='flex justify-center capitalize p-2'>metamask</p>
+      </div>
     </div>
   )
 }
